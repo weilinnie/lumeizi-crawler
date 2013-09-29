972e28e96f8ef556c1c40291228a9d0f1e85f847
winiex <winiex.nie@gamil.com>
2013-09-30 06:08:53 +0800

    Some kind of refactoring...

--- lu.py
+++ lu.py
@@ -4,16 +4,18 @@ import os
 from lumeizhi import Crawler, image_download
 
 
-if "data" not in os.listdir("."):
+if not os.path.isdir("./data"):
     os.mkdir("data")
 
 c = Crawler(0, 10)
 mm_no = 1
 
 for elem in c.crawl():
-    image_name = "MM" + str(mm_no) + ".jpg"
+    mm_name = "MM" + str(mm_no)
+    image_file_name = mm_name + ".jpg"
     image_url = "http://lumeizhi.com" + elem["src"]
-    print "Now we are on the way to meet " + image_name
-    image_download(image_url, image_name)
+
+    print "Now we are on the way to meet " + mm_name
+    image_download(image_url, image_file_name)
 
     mm_no += 1
